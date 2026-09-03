a8478f21f5f59c4e6a0d287b1e6a0f6a73b74dfb
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2026-09-03 21:04:59 +0530

    merge cleanup

---
+++ devenv.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+  devcontainer.enable = true;
+
+  packages = [ pkgs.git ];
+
+  env = {
+    PGUSER = "postgres";
+    PGPASSWORD = "postgres";
+    PGDATABASE = "app";
+  };
+
+  languages.python = {
+    enable = true;
+    lsp.enable = true;
+    venv.enable = true;
+    directory = "./data_generation";
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  languages.javascript = {
+    enable = true;
+    lsp.enable = true;
+    directory = "./mongo";
+  };
+
+  services.postgres = {
+    enable = true;
+    initdbArgs = [
+      "--locale=C"
+      "--encoding=UTF8"
+      "--username=${config.env.PGUSER}"
+    ];
+    initialDatabases = [
+      {
+        name = config.env.PGDATABASE;
+        user = config.env.PGUSER;
+        pass = config.env.PGPASSWORD;
+      }
+    ];
+  };
+
+  services.mongodb.enable = true;
+}
